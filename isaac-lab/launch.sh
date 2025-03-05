452041fc6c6006dec7afb088421153e323d6f0b9
Sheikh Dawood <sabdulajees@nvidia.com>
2025-03-05 17:39:35 -0600

    Revert changes

--- isaac-lab/launch.sh
+++ isaac-lab/launch.sh
@@ -1,17 +1,8 @@
-#!/usr/bin/env bash 
- 
-# Exits if error occurs 
-set -e 
- 
-# Install jupyter if needed 
-/isaac-sim/kit/python/bin/python3 -m pip install jupyter 
- 
-/isaac-sim/python.sh -m jupyter lab \ 
-  /workspace/isaaclab/livestream_sample.ipynb \ 
-  --allow-root \ 
-  --ip=0.0.0.0 \ 
-  --no-browser \ 
-  --NotebookApp.token='' \ 
-  --NotebookApp.password='' \ 
-  --NotebookApp.default_url='/tree/livestream_sample.ipynb' \ 
-  --NotebookApp.notebook_dir='/workspace/isaaclab'
+#!/usr/bin/env bash
+
+# Exits if error occurs
+set -e
+
+/isaac-sim/kit/python/bin/python3 -m pip install jupyter
+
+./_isaac_sim/python.sh -m jupyter lab /workspace/isaaclab/livestream_sample.ipynb --allow-root --ip=0.0.0.0 --no-browser --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.default_url='/tree/livestream_sample.ipynb' --NotebookApp.notebook_dir='/workspace/isaaclab'
